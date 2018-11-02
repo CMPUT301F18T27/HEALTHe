ace9e7c3381ff3a6a05236fac9ee7517650886d4
Paul Solleza <solleza@ualberta.ca>
2018-11-02 11:27:50 -0600

    created class UserList

---
+++ app/src/main/java/team27/healthe/model/UserList.java
@@ -0,0 +1,15 @@
+package team27.healthe.model;
+
+import java.util.Collection;
+
+public class UserList {
+    private Collection<User> users;
+
+    public void addUser(User u) {}
+
+    public User getUser(String userID) {
+        return null;
+    }
+
+    public void removeUser(User u) {}
+}
