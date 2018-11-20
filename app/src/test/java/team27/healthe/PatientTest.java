da93c0035e410503ae70792caf5bb6968061a5b9
Dodeler <cdodic@ualberta.ca>
2018-11-20 13:13:25 -0700

    fixed test errors

--- app/src/test/java/team27/healthe/PatientTest.java
+++ app/src/test/java/team27/healthe/PatientTest.java
@@ -23,32 +23,32 @@ public class PatientTest {
         assertEquals(phone, p.getPhone_number());
     }
 
-//    @Test
-//    public void addProblemTest() {
-//        Patient patient = new Patient("fromfuture", "ff@google.com", "7539521462");
-//        Integer problem = new Problem("i am a problem", new Date(), "I just got created");
-//
-//        patient.addProblem(problem);
-//        assertTrue(patient.hasProblem(problem));
-//    }
-//
-//    @Test
-//    public void getProblemTest() {
-//        Patient patient = new Patient("apple", "apple@google.com", "1515151515");
-//        String problemTitle = "im am green";
-//        Integer problem = new Problem(problemTitle, new Date(), "should I be red?");
-//        patient.addProblem(problem);
-//
-//        assertEquals(problem, patient.getProblem(problemTitle));
-//    }
-//
-//    @Test
-//    public void removeProblemTest() {
-//        Patient patient = new Patient("apple", "apple@google.com", "1515151515");
-//        Integer problem = new Problem("im am green", new Date(), "should I be red?");
-//        patient.addProblem(problem);
-//
-//        patient.removeProblem(problem);
-//        assertTrue(patient.getProblemList().isEmpty());
-//    }
+    @Test
+    public void addProblemTest() {
+        Patient patient = new Patient("fromfuture", "ff@google.com", "7539521462");
+        Problem problem = new Problem("i am a problem", new Date(), "I just got created");
+
+        patient.addProblem(problem.getProblemID());
+        assertTrue(patient.hasProblem(problem.getProblemID()));
+    }
+
+    @Test
+    public void getProblemTest() {
+        Patient patient = new Patient("apple", "apple@google.com", "1515151515");
+        String problemTitle = "im am green";
+        Problem problem = new Problem(problemTitle, new Date(), "should I be red?");
+        patient.addProblem(problem.getProblemID());
+
+        assertTrue(problem.equals(patient.getProblem(problemTitle)));
+    }
+
+    @Test
+    public void removeProblemTest() {
+        Patient patient = new Patient("apple", "apple@google.com", "1515151515");
+        Problem problem = new Problem("im am green", new Date(), "should I be red?");
+        patient.addProblem(problem.getProblemID());
+
+        patient.removeProblem(problem.getProblemID());
+        assertTrue(patient.getProblemList().isEmpty());
+    }
 }
