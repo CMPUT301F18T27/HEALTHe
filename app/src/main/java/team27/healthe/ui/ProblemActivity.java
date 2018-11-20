80364ac9c1b14f6f2aff62db6fb4693bb93ba375
tamaramicevic <tamara.micevic@gmail.com>
2018-11-20 12:02:19 -0700

    Add problem option has been added, not working with elastic search right now, needs testing

--- app/src/main/java/team27/healthe/ui/ProblemActivity.java
+++ app/src/main/java/team27/healthe/ui/ProblemActivity.java
@@ -2,7 +2,6 @@ package team27.healthe.ui;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -20,10 +19,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 import team27.healthe.R;
@@ -31,7 +30,6 @@ import team27.healthe.model.ElasticSearchController;
 import team27.healthe.model.LocalFileController;
 import team27.healthe.model.Problem;
 import team27.healthe.model.ProblemsAdapter;
-import team27.healthe.model.User;
 
 public class ProblemActivity extends AppCompatActivity {
     public static ListView listView;
@@ -54,6 +52,7 @@ public class ProblemActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                addProblem();
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -84,6 +83,81 @@ public class ProblemActivity extends AppCompatActivity {
 
         adapter = new ProblemsAdapter(this, problems);
         listView.setAdapter(adapter);
+    }
+
+    public void addProblem() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Add Problem");
+        dialog.setMessage("");
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        //Add title for problem
+        final TextView problem_title = new TextView(this);
+        problem_title.setText("Problem Title:");
+        layout.addView(problem_title);
+
+        // Add a TextView for problem title
+        final EditText title_text = new EditText(this);
+        title_text.setInputType(InputType.TYPE_CLASS_TEXT);
+        ViewGroup.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        ((LinearLayout.LayoutParams) params).setMargins(0,0,0,64);
+        title_text.setLayoutParams(params);
+        layout.addView(title_text); // Notice this is an add method
+
+        //Add title for date started
+        final TextView date_title = new TextView(this);
+        date_title.setText("Date Started:");
+        layout.addView(date_title);
+
+        // Add a TextView for date
+        final EditText date_text = new EditText(this);
+        date_text.setInputType(InputType.TYPE_CLASS_DATETIME);
+        layout.addView(date_text); // Another add method
+
+        //Add title for problem description
+        final TextView description_title = new TextView(this);
+        description_title.setText("Description:");
+        layout.addView(description_title);
+
+        // Add a TextView for description
+        final EditText description_text = new EditText(this);
+        description_text.setInputType(InputType.TYPE_TEXT_VARIATION_LONG_MESSAGE);
+        layout.addView(description_text); // Another add method
+
+        dialog.setView(layout);
+
+        dialog.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Adding problem...", Toast.LENGTH_SHORT).show();
+
+                String title = title_text.getText().toString();
+                Date date = new Date();
+                String desc = description_text.getText().toString();
+
+                try {
+                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+                    date = formatter.parse(date_text.getText().toString());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                Problem problem = new Problem(title, date, desc);
+
+                new AddProblemES().execute(problem);
+
+                LocalFileController file_controller = new LocalFileController(FILENAME);
+                file_controller.saveProblemInFile(problem, getApplicationContext());
+
+            }
+        })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                });
+        dialog.show();
     }
 
     public void deleteProblem(Problem prob) {
@@ -121,12 +195,12 @@ public class ProblemActivity extends AppCompatActivity {
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
 
-        //Add title for email input
+        //Add title for problem
         final TextView problem_title = new TextView(this);
-        problem_title.setText("Problem: ");
+        problem_title.setText("Problem Title:");
         layout.addView(problem_title);
 
-        // Add a TextView for email
+        // Add a TextView for problem title
         final EditText title_text = new EditText(this);
         title_text.setText(problem.getTitle());
         title_text.setInputType(InputType.TYPE_CLASS_TEXT);
@@ -135,18 +209,29 @@ public class ProblemActivity extends AppCompatActivity {
         title_text.setLayoutParams(params);
         layout.addView(title_text); // Notice this is an add method
 
-        //Add title for phone number input
+        //Add title for date started
         final TextView date_title = new TextView(this);
-        date_title.setText("Date Started: ");
+        date_title.setText("Date Started:");
         layout.addView(date_title);
 
-        // Add another TextView for phone number
+        // Add a TextView for date
         final EditText date_text = new EditText(this);
         date_text.setText(problem.getPdateAsString());
         date_text.setInputType(InputType.TYPE_CLASS_DATETIME);
         layout.addView(date_text); // Another add method
 
-        dialog.setView(layout); // Again this is a set method, not add
+        //Add title for problem description
+        final TextView description_title = new TextView(this);
+        description_title.setText("Description:");
+        layout.addView(description_title);
+
+        // Add a TextView for description
+        final EditText description_text = new EditText(this);
+        description_text.setText(problem.getDescription());
+        description_text.setInputType(InputType.TYPE_TEXT_VARIATION_LONG_MESSAGE);
+        layout.addView(description_text); // Another add method
+
+        dialog.setView(layout);
 
         dialog.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
@@ -154,6 +239,7 @@ public class ProblemActivity extends AppCompatActivity {
 
                 problem.setTitle(title_text.getText().toString());
                 problem.setPdateAsDateObj(date_text.getText().toString());
+                problem.setDescription(description_text.getText().toString());
 
                 new UpdateProblem().execute(problem);
 
@@ -168,6 +254,29 @@ public class ProblemActivity extends AppCompatActivity {
                     }
                 });
         dialog.show();
+    }
+
+    private class createAccountAsync extends AsyncTask<Problem, Void, Problem> {
+
+        @Override
+        protected Problem doInBackground(Problem... problems) {
+            ElasticSearchController es_controller = new ElasticSearchController();
+
+            for (Problem problem : problems) {
+                if (es_controller.getProblem(problem.getProblemID()) != null) { // If account already exists
+                    return null;
+                } else {
+                    es_controller.addProblem(problem);
+                    return problem;
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Problem problem) {
+            super.onPostExecute(problem);
+        }
     }
 
     private class DeleteProblem extends AsyncTask<Problem, Void, Problem> {
